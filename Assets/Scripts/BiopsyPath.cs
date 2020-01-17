3c4790c067653922a2d962f2fce35fed644a8f59
Luca Beisel <mail@lucabeisel.de>
2020-01-17 14:58:45 -0600

    Update BiopsyPath.cs

--- Assets/Scripts/BiopsyPath.cs
+++ Assets/Scripts/BiopsyPath.cs
@@ -4,12 +4,34 @@ using UnityEngine;
 
 public class BiopsyPath : MonoBehaviour
 {
+    public BiopsyManager manager;
     public LineRenderer lineRenderer;
     public SurgicalPointBiopsy biopsyPoint;
     public SurgicalPointEntry entryPoint;
 
+    public Material matRegular;
+    public Material matCorrent;
+    public Material matFalse;
+
     // Update is called once per frame
     void Update()
+    {
+        UpdatePosition();
+        UpdateAppearance();
+    }
+
+    public bool ToolInReach()
+    {
+        return false;
+    }
+
+    public bool CorrectAngle()
+    {
+        return false;
+    }
+
+
+    public void UpdatePosition()
     {
         if(biopsyPoint && entryPoint)
         {
@@ -20,6 +42,23 @@ public class BiopsyPath : MonoBehaviour
         else
         {
             lineRenderer.positionCount = 0;
+        }
+    }
+
+    public void UpdateAppearance()
+    {
+        if(!ToolInReach())
+        {
+            // regular
+
+        }
+        else if(ToolInReach() && CorrectAngle())
+        {
+            //green
+        }
+        else if(ToolInReach() && !CorrectAngle())
+        {
+            //red
         }
     }
 }
